2802ae4029010031023fb76a1851d7d01cdb356f
Tony Ibbs <tibs@kynesim.co.uk>
2014-05-06 14:40:29 +0100

    Make "getting commit ids" possible without printing

--- welded/query.py
+++ welded/query.py
@@ -8,25 +8,33 @@ import db
 import headers
 import ops
 
+def query_base_commits(spec, base_name):
+    # Find the last merge. This returns (None, None, []) if there wasn't one
+    last_weld_merge, last_base_merge, seams = headers.query_last_merge(spec.base_dir, base_name)
+    weld_init = git.query_init(spec.base_dir)
+    # Find the last push. Similar things happen if there wasn't one
+    last_weld_push, last_base_push, seams = headers.query_last_push(spec.base_dir, base_name)
+    # In order to determine the HEAD of our base, we need to make sure it is
+    # there (in .weld/bases/<base-name>)
+    b = spec.query_base(base_name)
+    ops.update_base(spec, b)
+    base_head = ops.query_head_of_base(spec, b)
+    return (last_weld_merge, last_base_merge, last_weld_push, last_base_push,
+            base_head, weld_init)
+
 def query_base(spec, base_name):
     """
     What is the latest commit on a given base?
     """
-    # Find the last merge. This returns (None, None, []) if there wasn't one
-    merge_id, base_merge_id, seams = headers.query_last_merge(spec.base_dir, base_name)
-    init_id = git.query_init(spec.base_dir)
-    # Find the last push. Similar things happen if there wasn't one
-    push_id, base_push_id, seams = headers.query_last_push(spec.base_dir, base_name)
-    b = spec.query_base(base_name)
-    ops.update_base(spec, b)
-    current_base_id = ops.query_head_of_base(spec, b)
+    (last_weld_merge, last_base_merge, last_weld_push, last_base_push,
+            base_head, weld_init) = query_base_commits(spec, base_name)
     print "Base %s"%base_name
-    print "  last merge, weld %s"%merge_id
-    print "              base %s"%base_merge_id
-    print "  last push,  weld %s"%push_id
-    print "              base %s"%base_push_id
-    print "  base HEAD  %s"%current_base_id
-    print "  weld Init  %s"%init_id
+    print "  last merge, weld %s"%last_weld_merge
+    print "              base %s"%last_base_merge
+    print "  last push,  weld %s"%last_weld_push
+    print "              base %s"%last_base_push
+    print "  base HEAD  %s"%base_head
+    print "  weld Init  %s"%weld_init
 
 def query_bases(spec):
     """Report on the bases we have, and their seams.
